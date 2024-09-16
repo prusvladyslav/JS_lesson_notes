77b2103cc434eb186449d24ea90b4fd76cac9dff
shisui <shisui@syneforge.com>
2024-09-16 20:51:42 +0300

    homework

---
+++ lesson4/homework.js
@@ -0,0 +1,47 @@
+// 1. циклом вывести в консоль каждую книгу каждого persona
+// const persons = [
+//     {
+//   name: "Vlad",
+//   age: 23,
+//   books: ["Harry Potter", "Lord of the Rings", "The Hobbit"],
+// }, 
+// {
+//     name: "Ivan",
+//     age: 25,
+//     books: ["War and Peace", "The Master and Margarita"],
+// },
+// {
+//     name: "Olga",
+//     age: 22,
+//     books: ["The Catcher in the Rye", "To Kill a Mockingbird"],
+// },
+// ]
+
+
+// 2. если у person age > 20, то добавить в массив книг еще 2 книги, если 18-20, то 1, если < 18, то 0
+
+// const persons = [
+//     {
+//   name: "Vlad",
+//   age: 23,
+//   books: ["Harry Potter", "Lord of the Rings", "The Hobbit"],
+// }, 
+// {
+//     name: "Ivan",
+//     age: 25,
+//     books: ["War and Peace", "The Master and Margarita"],
+// },
+// {
+//     name: "Olga",
+//     age: 22,
+//     books: ["The Catcher in the Rye", "To Kill a Mockingbird"],
+// },
+// ]
+
+// 3. реализовать функцию createBankAccount, которая принимает стартовый баланс и возвращает объект с методами deposit, withdraw, checkBalance (homework 3)
+// добавить метод addExpense, который будет принимать объект expense у которого будут поля amount:number, и добавлять его в массив expenses
+// добавить метод getExpenses, который будет возвращать все траты 
+// добавить метод removeLastExpense, который будет удалять последнюю трату, если трат нет, то выводить в консоль "No expenses"
+// добавить метод removeFirstExpense, который будет удалять первую трату, если трат нет, то выводить в консоль "No expenses"
+// добавить метод clearExpenses, который будет очищать массив expenses, если трат нет, то выводить в консоль "No expenses"
+// метод checkBalance должен возвращать баланс с учетом всех трат
